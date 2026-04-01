040de4a90fb4b644e29cf55eff89c4c04692b7e0
Mattias-Software <101396381+Mattias-Software@users.noreply.github.com>
2026-04-01 21:48:44 +0200

    added some simple boilerplate code

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+#include <conio.h>
+#include <c64/vic.h>
+#include <c64/keyboard.h>
 int main()
 {
-    printf("HELLO");
+    clrscr();
+    gotoxy(0,0);
+    printf("WELCOME TO GUNPEYC64\n");
+    printf("PRESS A KEY TO START\n");
+    getch();
+
 }
